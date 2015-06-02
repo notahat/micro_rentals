88e0f3fce99a376fc81bc499626612ab401837d0
Sebastian von Conrad <sebastian@vonconrad.com>
2015-06-02 11:52:52 +1000

    Simplify monitor

--- rental_offer/rental_offer_monitor.rb
+++ rental_offer/rental_offer_monitor.rb
@@ -6,31 +6,13 @@
 # To run monitor at prompt:
 #   ruby rental_car_monitor.rb 192.168.59.103 bugs
 
-require_relative 'connection'
+require_relative 'listener'
 
 # Streams rental-offer-related requests to the console
-class RentalOfferMonitor
-
-  def initialize(host, bus_name)
-    @host = host
-    @bus_name = bus_name
+class RentalOfferMonitor < Listener
+  def handle_packet(_, packet)
+    puts " [x] Received #{packet}"
   end
-
-  def start
-    Connection.with_open(@host, @bus_name) {|ch, ex| monitor_solutions(ch, ex) }
-  end
-
-private
-
-  def monitor_solutions(channel, exchange)
-    queue = channel.queue("", :exclusive => true)
-    queue.bind exchange
-    puts " [*] Waiting for solutions on the '#{@bus_name}' bus... To exit press CTRL+C"
-    queue.subscribe(block: true) do |delivery_info, properties, body|
-      puts " [x] Received #{body}"
-    end
-  end
-
 end
 
 RentalOfferMonitor.new(ARGV.shift, ARGV.shift).start
