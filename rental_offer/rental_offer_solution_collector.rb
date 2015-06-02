b63fecb5aeed9bd386df1004746fea5b2afaf140
Sebastian von Conrad <sebastian@vonconrad.com>
2015-06-02 11:46:33 +1000

    Make solution collector inherit from listener

--- rental_offer/rental_offer_solution_collector.rb
+++ rental_offer/rental_offer_solution_collector.rb
@@ -6,42 +6,20 @@
 # To run monitor at prompt:
 #   ruby rental_offer_monitor.rb 192.168.0.52 homer
 
-require_relative 'connection'
-require 'json'
-require 'pp'
+require_relative 'listener'
+
 # Streams rental-offer-related requests to the console
-class RentalOfferSolutionCollector
+class RentalOfferSolutionCollector < Listener
+  def handle_packet(exchange, packet)
+    return if packet['solution'].empty?
 
-  def initialize(host, bus_name)
-    @host = host
-    @bus_name = bus_name
-  end
-
-  def start
-    Connection.with_open(@host, @bus_name) {|ch, ex| monitor_solutions(ch, ex) }
-  end
-
-private
-
-  def monitor_solutions(channel, exchange)
-    queue = channel.queue("", :exclusive => true)
-    queue.bind exchange
-    puts " [*] Waiting for solutions on the '#{@bus_name}' bus... To exit press CTRL+C"
-    queue.subscribe(block: true) do |delivery_info, properties, body|
-      packet = JSON.parse(body)
-      if packet['json_class'] == 'RentalOfferSolutionPacket'
-        collect_solution(packet)
-        p [ packet['correlation_id'], db[packet['correlation_id']] ]
-      end
-    end
-  end
-
-  def collect_solution(packet)
     current_best_solution = db[packet['correlation_id']]
 
     if current_best_solution.nil? || packet['value'] > current_best_solution[:value]
-      db[packet['correlation_id']] = { description: packet['description'], value: packet['value'] }
+      db[packet['correlation_id']] = { description: packet['solution']['description'], value: packet['solution']['value'] }
     end
+
+    p [ packet['correlation_id'], db[packet['correlation_id']] ]
   end
 
   def db
