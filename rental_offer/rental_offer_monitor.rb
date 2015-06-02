eadb686d1b5f40875d98558542f642f103776cf7
Pete Yandell <pete@notahat.com>
2015-06-02 13:32:31 +1000

    Pretty print JSON in the monitor.

--- rental_offer/rental_offer_monitor.rb
+++ rental_offer/rental_offer_monitor.rb
@@ -11,7 +11,7 @@ require_relative 'listener'
 # Streams rental-offer-related requests to the console
 class RentalOfferMonitor < Listener
   def handle_packet(_, packet)
-    puts " [x] Received #{packet}"
+    puts " [x] Received #{JSON.pretty_generate(packet)}"
   end
 end
 
