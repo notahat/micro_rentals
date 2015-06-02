ca2c855888b1100dfb090beb439c72daea22d68c
Pete Yandell <pete@notahat.com>
2015-06-02 13:45:08 +1000

    Add hop counting to the listener.

--- rental_offer/listener.rb
+++ rental_offer/listener.rb
@@ -16,7 +16,10 @@ class Listener
     queue = channel.queue("", :exclusive => true)
     queue.bind exchange
     queue.subscribe(block: true) do |delivery_info, properties, body|
-      handle_packet(exchange, JSON.parse(body))
+      packet = JSON.parse(body)
+      packet['hops'] ||= 0
+      packet['hops'] += 1
+      handle_packet(exchange, packet) unless packet['hops'] > 9
     end
   end
 
