b0c729747b4f00bf1e335eb4fd275264cef987c7
Sebastian von Conrad <sebastian@vonconrad.com>
2015-06-02 13:42:24 +1000

    Add segment type provider

---
+++ rental_offer/rental_offer_segment_type_provider.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require_relative 'listener'
+
+class RentalOfferSegmentTypeProvider < Listener
+
+  def handle_packet(exchange, packet)
+    return if packet.has_key?('member_id') &&
+      !packet.has_key?('segment_type') &&
+      !packet.has_key?('solution')
+
+    packet['segment_type'] = [ 'road_warrior', 'resort_hound' ].sample
+
+    exchange.publish(packet.to_json)
+  end
+
+end
+
+RentalOfferSegmentTypeProvider.new(ARGV.shift, ARGV.shift).start
